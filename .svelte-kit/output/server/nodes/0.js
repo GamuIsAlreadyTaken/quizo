4445929e4921c7af6242caa732bfb997a4b5761a
GamuIsAlreadyTaken <GamuDiez@gmail.com>
2024-05-19 13:12:16 +0200

    removed dumb things

--- .svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.aa5-3Vdx.js","_app/immutable/chunks/scheduler.DHTTB31Y.js","_app/immutable/chunks/index.8iMzuK7F.js"];
-export const stylesheets = [];
-export const fonts = [];
